5142872e234f8174cdf7ca765ab7f45474f53a4a
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2016-12-16 23:05:34 +0300

    Initial commit

---
+++ src/main/groovy/d/kh/GStringVsString.groovy
@@ -0,0 +1,25 @@
+package d.kh
+
+/**
+ * Created by khamitovdm on 16/12/2016.
+ */
+class GStringVsString {
+    static void main(String[] args) {
+        def sKey = "a"
+        def gKey = "${sKey}"
+
+        def map1 = [:]
+        map1[sKey] = "A"
+        map1[gKey] = "A"
+
+        assert map1.size() == 1
+        println map1
+
+        def map2 = [(sKey): "A", (gKey): "A"]
+
+        assert map2.size() == 2
+        println map2
+
+        assert map1 != map2
+    }
+}
