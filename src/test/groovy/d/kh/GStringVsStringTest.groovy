dd0183a1e4c7493c6e3b4656a40156dcb8a72199
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2016-12-16 23:18:47 +0300

    Add placeholder for test

---
+++ src/test/groovy/d/kh/GStringVsStringTest.groovy
@@ -0,0 +1,14 @@
+package d.kh
+
+import org.junit.Test
+
+/**
+ * Created by khamitovdm on 16/12/2016.
+ */
+class GStringVsStringTest {
+    @Test
+    void 'nothing to test yet'() throws Exception {
+
+
+    }
+}
